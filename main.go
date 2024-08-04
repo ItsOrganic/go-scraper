8bcb0979bb5ea1e02327ec00e9ca85aedb162dc5
ItsOrganic <stutyashivam@gmail.com>
2024-08-05 00:19:46 +0530

    added flag -product=name_of_product to search that items on amazon

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 
@@ -10,14 +11,16 @@ import (
 
 func main() {
 	app := fiber.New()
-	var url string
-	fmt.Println("Enter the name of the product: ")
-	fmt.Scanln(&url)
-
-	app.Get("/scrape", func(c *fiber.Ctx) error {
-		products := crawler.ScrapeAmazon(url)
-		return c.JSON(products)
-	})
-	log.Fatal(app.Listen(":3000"))
-	fmt.Println("Listening on port :3000")
+	product := flag.String("product", "", "search a product by giving keyword")
+	flag.Parse()
+	if *product == "" {
+		log.Fatal("Flag is required")
+	} else {
+		app.Get("/", func(c *fiber.Ctx) error {
+			products := crawler.ScrapeAmazon(*product)
+			return c.JSON(products)
+		})
+		log.Fatal(app.Listen(":3000"))
+		fmt.Println("Listening on port :3000")
+	}
 }
